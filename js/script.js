ea15ac0bd4afba7687af6bc12dd205a891fdc874
Muhammad Fadhil Swarigading <117817741+padhlsg@users.noreply.github.com>
2023-12-23 15:13:23 +0700

    Update Portofoolio
    Mengganti Tema dari website

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 window.addEventListener("scroll", function () {
   let navbar = document.querySelector("nav");
-  navbar.classList.toggle("sticky", window.scrollY > 0);
+  navbar.classList.toggle("sticky", window.scrollY > 720);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Reference to the container and the original set of projects
+  const projects = document.querySelectorAll(".project");
+
+  // Clone the original set of projects
+  const cloneProjects = Array.from(projects).map((project) => project.cloneNode(true));
+
+  // Append the cloned projects to the container
 });
 
 AOS.init();
